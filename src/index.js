5a00c960f0c874347e82ec3dfbee93ce3968292f
ChristopherDalziel <Christopher_Dalziel@hotmail.com>
2021-09-10 16:59:29 +1000

    cleared out create-react-app garbage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 import { createStore } from "redux";
 import { Provider } from "react-redux";
@@ -20,8 +18,3 @@ const App = (
 );
 
 ReactDOM.render(App, document.getElementById("root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
