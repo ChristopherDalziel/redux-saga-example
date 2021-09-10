dc0da328f4c636608838c9b0ce8002bad4f82b15
ChristopherDalziel <Christopher_Dalziel@hotmail.com>
2021-09-10 17:13:15 +1000

    resolved issue where onClick wasn't firing

--- src/components/CounterComponent.js
+++ src/components/CounterComponent.js
@@ -5,8 +5,8 @@ export default class CounterComponent extends Component {
     return (
       <div>
         <h1>Counter - Redux Saga Flow Example</h1>
-        <button onClick={this.props.increment(1)}>Increment + 1</button>
-        <button onClick={this.props.decrement(1)}>Decrement - 1</button>
+        <button onClick={() => this.props.increment(1)}>Increment + 1</button>
+        <button onClick={() => this.props.decrement(1)}>Decrement - 1</button>
         <div>Count: {this.props.value}</div>
       </div>
     );
